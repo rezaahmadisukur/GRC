8f62aa269a9293faa145d9180128d85811134344
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-04-29 20:29:05 +0700

    refactor: delete route not using

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,6 @@ Route::post('/check-booking', [BookingController::class, 'check'])->name('bookin
 Route::middleware(['auth'])->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/password', [ProfileController::class, 'updatePassword'])->name('password.update');
 
     Route::get('/force-change-password', [ProfileController::class, 'showForceChangePassword'])->name('password.force-change');
