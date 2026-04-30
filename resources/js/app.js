107884f439389893b3cfc8b7d39134cef328a4c5
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-04-30 20:52:03 +0700

    refactor: delete sweet alert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,11 @@
 import "./bootstrap";
 import Alpine from "alpinejs";
 import Chart from "chart.js/auto";
-// import Swal from "sweetalert2";
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.css";
 import { Indonesian } from "flatpickr/dist/l10n/id.js";
 
 window.flatpickr = flatpickr;
-// window.Swal = Swal;
 window.Chart = Chart;
 window.Alpine = Alpine;
 Alpine.start();
