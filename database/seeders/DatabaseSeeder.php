d0c6d82d99f7f8cb4e98fe3555e50be3bbf44b97
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-05-01 06:40:06 +0700

    refactor: comment car seeder and booking seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            CarSeeder::class,
-            BookingSeeder::class,
+            // CarSeeder::class,
+            // BookingSeeder::class,
         ]);
     }
 }
