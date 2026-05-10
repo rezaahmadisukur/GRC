e296f62764cb4d5d8348f3a7347a0ff448588199
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-05-10 15:14:11 +0700

    feat: register anonymous component path for admin cars components

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useTailwind();
+        Blade::anonymousComponentPath(resource_path('views/admin/cars/components'));
     }
 }
