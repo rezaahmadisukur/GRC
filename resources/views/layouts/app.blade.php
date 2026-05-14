816e27d43783c030300d33d0c4800f4764b591af
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-05-14 09:07:45 +0700

    feat(layouts): add dynamic title prop to app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,5 @@
+@props(['title'])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
@@ -6,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ isset($title) ? "$title - " . config('app.name') : config('app.name') }}</title>
 
     <!-- Fav Icon -->
     @include('layouts.favicon')
