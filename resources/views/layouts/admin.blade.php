3bf8dff18ddf76d8ccb4a163ec61539da39d8d0e
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-05-14 08:26:44 +0700

    refactor(admin layout): replace Google Fonts with favicon include

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,9 +7,10 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('app.name', 'Laravel') }}</title>
 
-  <link rel="preconnect" href="https://fonts.bunny.net">
-  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <!-- Fav Icon -->
+  @include('layouts.favicon')
 
+  <!-- Scripts -->
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 
   <style>
